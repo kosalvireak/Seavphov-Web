8ef5d0360c4ae25d010fdf965df6a0d663626ad0
Kosalvireak <virakvary@gmail.com>
2024-04-25 10:33:42 +0700

    style singnup

--- seavphov/src/view/Signup.vue
+++ seavphov/src/view/Signup.vue
@@ -1,119 +1,115 @@
 <template>
-  <div class="main-container">
-    <div class="row">
-      <div
-        class="d-flex align-items-center justify-content-center logo col-md-6 col-sm-12"
-      >
-        <img src="/img/book.png" alt="booklogo" class="logoimg img-fluid" />
-      </div>
-      <div class="container col-md-6 col-sm-12">
-        <h1>Sign Up</h1>
-        <form v-on:submit.prevent="Signup()">
-          <div class="form-floating mb-3">
-            <input
-              type="name"
-              class="form-control btn rounded-pill text-start"
-              id="name"
-              placeholder="name"
-              v-model="name"
-              style="background-color: #d9d9d9"
-              @focus="toggleLabel('name', true)"
-              @blur="toggleLabel('name', false)"
-              required
-            />
-            <label for="name" :class="{ 'special-style': toggleEmail }"
-              >Name</label
-            >
-          </div>
-          <div class="form-floating mb-3">
-            <input
-              type="email"
-              class="form-control btn rounded-pill text-start"
-              id="email"
-              placeholder="name@example.com"
-              v-model="email"
-              style="background-color: #d9d9d9"
-              @focus="toggleLabel('email', true)"
-              @blur="toggleLabel('email', false)"
-              required
-            />
-            <label for="email" :class="{ 'special-style': toggleEmail }"
-              >Email address</label
-            >
-          </div>
-          <div class="form-floating password mb-3">
-            <input
-              type="password"
-              class="form-control btn rounded-pill text-start"
-              id="password"
-              placeholder="password"
-              v-model="password"
-              style="background-color: #d9d9d9"
-              @focus="toggleLabel('password', true)"
-              @blur="toggleLabel('password', false)"
-              required
-            />
-            <label for="password" :class="{ 'special-style': togglePassword }"
-              >Password</label
-            >
-          </div>
-          <div class="form-floating mb-3">
-            <input
-              type="password"
-              class="form-control btn rounded-pill text-start"
-              id="password_confirmation"
-              placeholder="password_confirmation"
-              v-model="password_confirmation"
-              style="background-color: #d9d9d9"
-              @focus="toggleLabel('password_confirmation', true)"
-              @blur="toggleLabel('password_confirmation', false)"
-              required
-            />
-            <label
-              for="password_confirmation"
-              :class="{ 'special-style': toggleConfirmPassword }"
-              >Confirm Password</label
-            >
-          </div>
-          <div class="d-flex align-items-start justify-content-between my-3">
-            <div class="d-flex justify-content-center ps-4 align-items-start">
-              <!-- Checkbox -->
-              <div
-                class="form-check h-100 d-flex align-items-start justify-content-center"
-              >
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  value=""
-                  id="form1Example3"
-                  checked
-                  v-model="isShowPassword"
-                  @click="showPassword"
-                />
-                <label class="form-check-label" for="form1Example3">
-                  Show password
-                </label>
-              </div>
-            </div>
-          </div>
-          <p v-if="Error" class="text-danger">{{ errorMessage }}</p>
-          <button type="submit" class="btn btn-primary btn-block">
-            Sign up
-          </button>
-        </form>
-
-        <div class="text-center mt-3">
-          Already have an account?
-          <a
-            @click="
-              () => {
-                this.$router.push('/login');
-              }
-            "
-            class="text-seavphov"
-            >Login.</a
+  <div class="row">
+    <div
+      class="d-flex align-items-center justify-content-center logo col-md-6 col-sm-12"
+    >
+      <img src="/img/book.png" alt="booklogo" class="logoimg img-fluid" />
+    </div>
+    <div class="container col-md-6 col-sm-12">
+      <h1>Sign Up</h1>
+      <form v-on:submit.prevent="Signup()">
+        <div class="form-floating mb-3">
+          <input
+            type="name"
+            class="form-control btn rounded-pill text-start"
+            id="name"
+            placeholder="name"
+            v-model="name"
+            style="background-color: #d9d9d9"
+            @focus="toggleLabel('name', true)"
+            @blur="toggleLabel('name', false)"
+            required
+          />
+          <label for="name" :class="{ 'special-style': toggleEmail }"
+            >Name</label
           >
         </div>
+        <div class="form-floating mb-3">
+          <input
+            type="email"
+            class="form-control btn rounded-pill text-start"
+            id="email"
+            placeholder="name@example.com"
+            v-model="email"
+            style="background-color: #d9d9d9"
+            @focus="toggleLabel('email', true)"
+            @blur="toggleLabel('email', false)"
+            required
+          />
+          <label for="email" :class="{ 'special-style': toggleEmail }"
+            >Email address</label
+          >
+        </div>
+        <div class="form-floating password mb-3">
+          <input
+            type="password"
+            class="form-control btn rounded-pill text-start"
+            id="password"
+            placeholder="password"
+            v-model="password"
+            style="background-color: #d9d9d9"
+            @focus="toggleLabel('password', true)"
+            @blur="toggleLabel('password', false)"
+            required
+          />
+          <label for="password" :class="{ 'special-style': togglePassword }"
+            >Password</label
+          >
+        </div>
+        <div class="form-floating mb-3">
+          <input
+            type="password"
+            class="form-control btn rounded-pill text-start"
+            id="password_confirmation"
+            placeholder="password_confirmation"
+            v-model="password_confirmation"
+            style="background-color: #d9d9d9"
+            @focus="toggleLabel('password_confirmation', true)"
+            @blur="toggleLabel('password_confirmation', false)"
+            required
+          />
+          <label
+            for="password_confirmation"
+            :class="{ 'special-style': toggleConfirmPassword }"
+            >Confirm Password</label
+          >
+        </div>
+        <div class="d-flex align-items-start justify-content-between my-3">
+          <div class="d-flex justify-content-center ps-4 align-items-start">
+            <!-- Checkbox -->
+            <div
+              class="form-check h-100 d-flex align-items-start justify-content-center"
+            >
+              <input
+                class="form-check-input"
+                type="checkbox"
+                value=""
+                id="form1Example3"
+                checked
+                v-model="isShowPassword"
+                @click="showPassword"
+              />
+              <label class="form-check-label" for="form1Example3">
+                Show password
+              </label>
+            </div>
+          </div>
+        </div>
+        <p v-if="Error" class="text-danger">{{ errorMessage }}</p>
+        <button type="submit" class="btn btn-primary btn-block">Sign up</button>
+      </form>
+
+      <div class="text-center mt-3">
+        Already have an account?
+        <a
+          @click="
+            () => {
+              this.$router.push('/login');
+            }
+          "
+          class="text-seavphov"
+          >Login.</a
+        >
       </div>
     </div>
   </div>
@@ -191,17 +187,11 @@ export default {
 </script>
   
 <style scoped>
-.main-container {
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
 .row {
   background-color: #fff;
   border-radius: 30px;
   width: 900px;
-  padding: 50px;
+  padding: 20px;
 }
 
 .logo {
@@ -244,7 +234,7 @@ button {
   background-color: #5c836e;
   color: #fff;
   padding: 7px 70px;
-  width: 300px;
+  max-width: 300px;
   margin: auto;
   border-radius: 15px;
   font-weight: bold;
