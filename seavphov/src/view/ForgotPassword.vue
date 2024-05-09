8897fd38c894b66b0085f134d70b7d5dcfed3c47
Kosalvireak <virakvary@gmail.com>
2024-05-09 12:06:48 +0700

    reset password ui

--- seavphov/src/view/ForgotPassword.vue
+++ seavphov/src/view/ForgotPassword.vue
@@ -1,12 +1,26 @@
 <template>
   <div
-    class="ForgotPassword shadow-5 d-flex-center flex-column col-md-6 col-sm-12 justify-content-between m-3"
+    class="ForgotPassword shadow-5 d-flex-center flex-column col-md-6 col-sm-12 justify-content-between m-3 p-2"
   >
-    <h2 class="mt-2">Forgot Password</h2>
+    <div
+      class="ForgotPassword_top border-bottom d-flex-center justify-content-between"
+    >
+      <span>
+        <i
+          @click="switchPage()"
+          class="fa fa-arrow-circle-left fa-xl ms-3 cursor-pointer"
+        ></i>
+      </span>
+      <h2 class="mt-2 me-3">Forgot Password</h2>
+      <span></span>
+    </div>
 
-    <div v-if="page == 1" class="d-flex-center flex-column">
+    <div
+      v-if="page == 1"
+      class="SendEmailForm d-flex-center flex-column mb-7 w-60"
+    >
       <h5 class="my-3">
-        Enter your email address we will send a token for reset your password .
+        Enter your email address we will send a token for reset your password.
       </h5>
       <form v-on:submit.prevent="SendEmail()" class="w-100">
         <div class="form-floating mb-3">
@@ -31,24 +45,83 @@
         </div>
       </form>
     </div>
-
     <div
-      class="ForgotPassword_buttom border-top d-flex-center justify-content-between"
+      v-if="page == 2"
+      class="ResetPasswordForm d-flex-center flex-column my-3 w-60"
     >
-      <span>
-        <i
-          v-if="page == 2"
-          @click="switchPage(1)"
-          class="fa fa-arrow-circle-left fa-xl ms-3 cursor-pointer"
-        ></i>
-      </span>
-      <span>
-        <i
-          v-if="page == 1"
-          @click="switchPage(2)"
-          class="fa fa-arrow-circle-right fa-xl me-3 cursor-pointer"
-        ></i>
-      </span>
+      <h5 class="my-3">Enter the token we sent to your email address.</h5>
+      <form v-on:submit.prevent="SendResetPassword()" class="w-100">
+        <div class="form-floating mb-3">
+          <MDBInput
+            type="email"
+            label="Email"
+            id="email"
+            v-model="email"
+            wrapperClass="bg-white p-2"
+            required
+          />
+        </div>
+        <div class="form-floating password mb-3">
+          <MDBInput
+            type="text"
+            label="Token"
+            id="token"
+            v-model="token"
+            wrapperClass="bg-white p-2"
+            required
+          />
+        </div>
+        <div class="form-floating password mb-3">
+          <MDBInput
+            type="password"
+            label="Password"
+            id="password"
+            v-model="password"
+            wrapperClass="bg-white p-2"
+            required
+          />
+        </div>
+        <div class="form-floating mb-3">
+          <MDBInput
+            type="password"
+            label="Confirm Password"
+            id="password_confirmation"
+            v-model="password_confirmation"
+            wrapperClass="bg-white p-2"
+            required
+          />
+        </div>
+        <div class="d-flex align-items-start justify-content-between my-3">
+          <div class="d-flex justify-content-center align-items-start">
+            <div
+              class="form-check h-100 d-flex align-items-start justify-content-center"
+            >
+              <input
+                class="form-check-input"
+                type="checkbox"
+                value=""
+                id="form1Example3"
+                checked
+                v-model="isShowPassword"
+                @click="showPassword"
+              />
+              <label class="form-check-label" for="form1Example3">
+                Show password
+              </label>
+            </div>
+          </div>
+        </div>
+        <p v-if="Error" class="text-danger">{{ errorMessage }}</p>
+        <div class="form-floating d-flex-center justify-content-end">
+          <button
+            type="submit"
+            class="btn btn-primary mt-2 d-flex-center btn_submit"
+          >
+            <span v-if="!isLoading">Reset</span>
+            <Loader v-else :size="20" :Color="'#FFFFFF'" />
+          </button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -63,20 +136,43 @@ export default {
   data() {
     return {
       email: "virakvary@gmail.com",
+      token: "1234",
+      password: "12345678",
+      password_confirmation: "12345678",
       isLoading: false,
       Error: false,
       errorMessage: "",
-      page: "2",
+      isShowPassword: false,
+      page: "1",
     };
   },
   methods: {
     async SendEmail() {
-      this.isLoading = true;
-      await this.$store.dispatch("registerUser", signupData);
-      this.isLoading = false;
+      this.page = 2;
+      //.isLoading = true;
+      // await this.$store.dispatch("registerUser", signupData);
+      //this.isLoading = false;
     },
-    switchPage(page) {
-      this.page = page;
+    async SendResetPassword() {
+      if (this.page == 2) {
+        this.$router.push("/login");
+      }
+    },
+    showPassword() {
+      if (this.isShowPassword) {
+        password.type = "password";
+        password_confirmation.type = "password";
+      } else {
+        password.type = "text";
+        password_confirmation.type = "text";
+      }
+    },
+    switchPage() {
+      if (this.page == 1) {
+        this.$router.push("/login");
+      } else if (this.page == 2) {
+        this.page = 1;
+      }
     },
   },
 };
@@ -85,10 +181,10 @@ export default {
 <style>
 .ForgotPassword {
   max-width: 750px;
-  min-height: 400px;
+  min-height: 400px !important;
   margin-bottom: 200px;
 }
-.ForgotPassword_buttom {
+.ForgotPassword_top {
   height: 50px;
   width: 100%;
 }
