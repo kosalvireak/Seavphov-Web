968a643c012ad6e8876b3b1aed3e2c264b5da92d
DarwitChea <cheadaracheadavid@gmail.com>
2024-05-17 12:14:32 +0700

    Added Carousels

--- seavphov/src/components/Carousel.vue
+++ seavphov/src/components/Carousel.vue
@@ -1,76 +1,108 @@
 <template>
-  <div
-    id="carouselExampleInterval"
-    class="carousel slide w-100 h-100 rounded-7"
-    data-bs-ride="carousel"
-  >
-    <div class="carousel-inner carousel-img">
-      <div class="carousel-item active carousel-img" data-bs-interval="5000">
-        <img
-          src="/img/cover0.jpg"
-          class="d-block w-100 rounded-7"
-          alt="Wild Landscape"
-          style="object-fit: cover; height: 300px"
-        />
-      </div>
-      <div class="carousel-item carousel-img" data-bs-interval="5000">
-        <img
-          src="/img/cover1.jpg"
-          class="d-block w-100 rounded-7"
-          alt="Wild Landscape"
-          style="object-fit: cover; height: 300px"
-        />
-      </div>
-      <div class="carousel-item carousel-img" data-bs-interval="5000">
-        <img
-          src="/img/cover2.jpg"
-          class="d-block w-100 rounded-7"
-          alt="Wild Landscape"
-          style="object-fit: cover; height: 300px"
-        />
-      </div>
-      <div class="carousel-item carousel-img" data-bs-interval="5000">
-        <img
-          src="/img/cover3.jpg"
-          class="d-block w-100 rounded-7 carousel-img"
-          alt="Wild Landscape"
-          style="object-fit: cover; height: 300px"
-        />
+  <div id="carouselExampleInterval" class="carousel slide w-100 h-500 rounded-7" data-bs-ride="carousel"
+    data-bs-interval="5000">
+    <div class="carousel-inner carousel-img" style="height: auto;">
+      <div v-for="(book, index) in books" :key="index" :class="{ 'carousel-item': true, 'active': index === 0 }">
+        <div class="row align-items-start m-2">
+          <div class="col">
+            <h1 class="mb-0">{{ book.title }}</h1>
+            <div class="row my-3 ">
+              <div class="col d-flex flex-row">
+                <h5 class="me-3">{{ book.author }}</h5>
+                <h5>Condition: {{ book.condition }}</h5>
+              </div>
+            </div>
+            <div class="description">{{ book.descriptions }}</div>
+
+            <div class="col my-3">
+              <router-link :to="`/book/${book.id}`" class="d-flex justify-content-start h-50  hover-zoom"
+                style="height: 13rem">
+                <button type="button" class="btn">READ MORE</button>
+              </router-link>
+
+            </div>
+          </div>
+
+          <div class="col">
+            <img :src="book.images" class="d-block w-100 rounded-7" alt="Book Cover"
+              style="object-fit: contain; width:200px; height: 300px;">
+          </div>
+        </div>
       </div>
     </div>
-    <button
-      class="carousel-control-prev carousel-img"
-      type="button"
-      data-bs-target="#carouselExampleInterval"
-      data-bs-slide="prev"
-    >
+
+    <div class="carousel-indicators d-flex justify-content-center mt-3">
+      <button v-for="(book, index) in books" :key="index" type="button" :data-bs-target="'#carouselExampleInterval'"
+        :data-bs-slide-to="index" :class="{ 'active': index === 0 }" aria-current="true"
+        :aria-label="'Slide ' + (index + 1)"></button>
+    </div>
+
+    <!-- <button class="carousel-control-prev carousel-img" type="button" data-bs-target="#carouselExampleInterval"
+      data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
     </button>
-    <button
-      class="carousel-control-next carousel-img"
-      type="button"
-      data-bs-target="#carouselExampleInterval"
-      data-bs-slide="next"
-    >
+    <button class="carousel-control-next carousel-img" type="button" data-bs-target="#carouselExampleInterval"
+      data-bs-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
-    </button>
+    </button> -->
   </div>
 </template>
-  
-  <script>
-export default {};
+
+<script>
+export default {
+  name: "Book",
+  props: {
+    books: Array, // Change Object to Array for the props
+  },
+};
 </script>
-  
-  <style scoped>
+
+
+<style scoped>
 .carousel-img {
-  height: 300px !important;
+  /* height: 300px !important; */
   border-radius: 1rem;
 }
+
 @media (max-width: 576px) {
   .carousel-img {
     height: 250px !important;
   }
+}
+
+h1 {
+  color: white;
+  font-weight: bold;
+  max-width: 700px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+h5 {
+  color: white;
+}
+
+.description {
+  color: lightgray;
+  max-width: 600px;
+  max-height: 100px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 4;
+  white-space: normal;
+}
+
+.btn {
+  color: white;
+  background-color: #9db673;
+}
+
+.carousel-indicators {
+  position: relative !important;
 }
 </style>
--- seavphov/src/view/Home.vue
+++ seavphov/src/view/Home.vue
@@ -1,13 +1,12 @@
 <template>
-
-  <!--Homepage Image -->
   <div class="w-100 h-100">
-    <div class="container-sm d-flex-center my-3" style="width: 100%">
-      <img
-        src="/img/login_background.png"
-        class="img-fluid"
-        style="max-height: 400px; object-fit: cover; width: 100%"
-      />
+
+    <!--Homepage Image -->
+    <div class="backgroundImg d-flex-center img-fluid rounded-7" style="width: 100%; height:500px">
+      <div class="d-flex-center p-3 w-75 h-100">
+        <Carousel v-if="TopBooks.length" :books="TopBooks" />
+        <!-- <div>{{ TopBooks }}</div> -->
+      </div>
     </div>
 
     <!-- Filter -->
@@ -16,31 +15,19 @@
       <PaginatedBook />
     </div> -->
     <div class="mt-3 row" v-if="false">
-      <div
-        class="d-flex align-item-center justify-content-center col-xl-3 col-md-5 col-sm-12 mt-md-2"
-      >
+      <div class="d-flex align-item-center justify-content-center col-xl-3 col-md-5 col-sm-12 mt-md-2">
         <div
           class="d-flex align-item-center justify-content-center flex-column w-100 rounded-7 mt-md-5 px-2 bg-seavphov-light"
-          style="height: 31rem"
-          :class="{ 'h-3rem': toggleFilter }"
-        >
-          <div
-            class="ms-3 d-flex align-items-start justify-content-between"
-            :class="{ 'margin-top': toggleFilter }"
-            style="display: flex"
-          >
+          style="height: 31rem" :class="{ 'h-3rem': toggleFilter }">
+          <div class="ms-3 d-flex align-items-start justify-content-between" :class="{ 'margin-top': toggleFilter }"
+            style="display: flex">
             <div class="d-flex align-items-center justify-content-center">
               <h4 class="text-center text-gray fw-bold text-seavphov">
                 Filter
               </h4>
             </div>
-            <div
-              @click="ToggleFilter()"
-              class="d-flex justify-content-center me-2"
-            >
-              <span v-if="toggleFilter"
-                ><i class="fas fa-arrow-down fa-xl"></i
-              ></span>
+            <div @click="ToggleFilter()" class="d-flex justify-content-center me-2">
+              <span v-if="toggleFilter"><i class="fas fa-arrow-down fa-xl"></i></span>
               <span v-else><i class="fas fa-arrow-up fa-xl"></i></span>
             </div>
           </div>
@@ -49,127 +36,51 @@
             <div class="category ms-3">
               <h5 class="fw-bold text-gray">Category</h5>
               <div class="form-check">
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  v-model="Categories"
-                  value="Fiction"
-                  id="Fiction"
-                />
+                <input class="form-check-input" type="checkbox" v-model="Categories" value="Fiction" id="Fiction" />
                 <label class="form-check-label" for="Fiction">Fiction</label>
                 <br />
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  v-model="Categories"
-                  value="Novel"
-                  id="Novel"
-                />
+                <input class="form-check-input" type="checkbox" v-model="Categories" value="Novel" id="Novel" />
                 <label class="form-check-label" for="Novel">Novel</label><br />
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  v-model="Categories"
-                  value="Text-Book"
-                  id="Text-Book"
-                />
-                <label class="form-check-label" for="Text-Book">Text-Book</label
-                ><br />
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  v-model="Categories"
-                  value="History"
-                  id="History"
-                />
-                <label class="form-check-label" for="History">History</label
-                ><br />
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  v-model="Categories"
-                  value="Science"
-                  id="Science"
-                />
-                <label class="form-check-label" for="Science">Science</label
-                ><br />
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  v-model="Categories"
-                  value="Fantasy"
-                  id="Fantasy"
-                />
-                <label class="form-check-label" for="Fantasy">Fantasy</label
-                ><br />
+                <input class="form-check-input" type="checkbox" v-model="Categories" value="Text-Book" id="Text-Book" />
+                <label class="form-check-label" for="Text-Book">Text-Book</label><br />
+                <input class="form-check-input" type="checkbox" v-model="Categories" value="History" id="History" />
+                <label class="form-check-label" for="History">History</label><br />
+                <input class="form-check-input" type="checkbox" v-model="Categories" value="Science" id="Science" />
+                <label class="form-check-label" for="Science">Science</label><br />
+                <input class="form-check-input" type="checkbox" v-model="Categories" value="Fantasy" id="Fantasy" />
+                <label class="form-check-label" for="Fantasy">Fantasy</label><br />
               </div>
             </div>
             <div class="condition ms-3">
               <h5 class="fw-bold text-gray">Condition</h5>
               <div class="form-check">
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  v-model="Condition"
-                  value="As-new"
-                  id="As-new"
-                />
+                <input class="form-check-input" type="checkbox" v-model="Condition" value="As-new" id="As-new" />
                 <label class="form-check-label" for="As-new">As-new</label>
                 <br />
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  v-model="Condition"
-                  value="Good"
-                  id="Good"
-                />
+                <input class="form-check-input" type="checkbox" v-model="Condition" value="Good" id="Good" />
                 <label class="form-check-label" for="Good">Good</label><br />
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  v-model="Condition"
-                  value="Well-worn"
-                  id="Well-worn"
-                />
-                <label class="form-check-label" for="Well-worn"
-                  >Well-worn</label
-                >
+                <input class="form-check-input" type="checkbox" v-model="Condition" value="Well-worn" id="Well-worn" />
+                <label class="form-check-label" for="Well-worn">Well-worn</label>
               </div>
             </div>
             <div class="availability ms-3">
               <h5 class="fw-bold text-gray">Availability</h5>
               <div class="form-check">
-                <input
-                  class="form-check-input"
-                  type="radio"
-                  name="Availability"
-                  v-model="Availability"
-                  value="true"
-                  id="Avaliable"
-                />
+                <input class="form-check-input" type="radio" name="Availability" v-model="Availability" value="true"
+                  id="Avaliable" />
                 <label class="form-check-label" for="Avaliable">
                   Avaliable
                 </label>
                 <br />
-                <input
-                  class="form-check-input"
-                  type="radio"
-                  name="Availability"
-                  v-model="Availability"
-                  value="false"
-                  id="NotAvaliable"
-                />
+                <input class="form-check-input" type="radio" name="Availability" v-model="Availability" value="false"
+                  id="NotAvaliable" />
                 <label class="form-check-label" for="NotAvaliable">
                   Not Avaliable
                 </label>
                 <br />
               </div>
             </div>
-            <button
-              class="btn btn-danger mt-2 ms-3"
-              @click="clearFilter()"
-              style="width: 6rem"
-            >
+            <button class="btn btn-danger mt-2 ms-3" @click="clearFilter()" style="width: 6rem">
               clear
             </button>
           </div>
@@ -186,12 +97,18 @@
 import RenderBook from "../components/RenderBook.vue";
 import PaginatedBook from "../components/PaginatedBook.vue";
 import Filter from "../components/Filter.vue";
+import Carousel from "../components/Carousel.vue"
+
 export default {
   name: "Home",
-  components: { RenderBook, PaginatedBook, Filter },
+  components: { RenderBook, PaginatedBook, Filter, Carousel },
   data() {
     return {
       reloaded: false,
+      filters: {
+        categories: "Text-Book",
+      },
+      TopBooks: []
     };
   },
   // computed: {
@@ -234,21 +151,28 @@ export default {
   //     return this.$store.state.fetchBooks;
   //   },
   // },
-  // methods: {
-  //   ToggleFilter() {
-  //     this.toggleFilter = !this.toggleFilter;
-  //   },
-  //   // reset filtered array and reset availability
-  //   clearFilter() {
-  //     this.Categories = [];
-  //     this.Condition = [];
-  //     this.Availability = null;
-  //   },
-  //   onChangePage(currentPage) {
-  //     console.log("currentPage", currentPage);
-  //   },
-  // },
-  mounted() {
+  methods: {
+    // ToggleFilter() {
+    //   this.toggleFilter = !this.toggleFilter;
+    // },
+    // // reset filtered array and reset availability
+    // clearFilter() {
+    //   this.Categories = [];
+    //   this.Condition = [];
+    //   this.Availability = null;
+    // },
+    // onChangePage(currentPage) {
+    //   console.log("currentPage", currentPage);
+    // },
+    async getBook() {
+      this.TopBooks = await this.$store.dispatch(
+        "fetchBooksWithFilter",
+        this.filters
+      );
+    }
+  },
+  async mounted() {
+    this.getBook(this.paramsId);
     if (!localStorage.getItem("reloaded")) {
       location.reload();
       localStorage.setItem("reloaded", true);
@@ -261,10 +185,18 @@ export default {
 label {
   font-size: 0.9rem;
 }
+
 .h-3rem {
   height: 3rem !important;
 }
+
 .margin-top {
   margin-top: 20px !important;
+}
+
+.backgroundImg {
+  background-image: url('/img/login_background.png');
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
