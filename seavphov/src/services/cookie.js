66ff7dbf92c74d5a6016e174a03fe279b50a6fc2
Kosal Vireak <kosalvireak.khoeun@khalibre.com>
2025-02-25 11:02:31 +0700

    refactor: move file to service folder

--- seavphov/src/services/cookie.js
+++ seavphov/src/services/cookie.js
@@ -15,8 +15,7 @@ export function getCookie() {
   const user = VueCookies.get("seavphov");
   if (user) {
     const bytes = CryptoJS.AES.decrypt(user, secretKey);
-    const data = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-    return data;
+    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
   } else {
     return null;
   }
