d52f13dd545afed4a2912b19de17dd16d83132e2
Kosal Vireak <kosalvireak.khoeun@khalibre.com>
2025-04-02 10:43:13 +0700

    refactor: handle success and error response in axios middleware

--- seavphov/src/services/axiosInstance.js
+++ seavphov/src/services/axiosInstance.js
@@ -7,36 +7,42 @@ const axiosInstance = axios.create({
   timeout: 30000,
 });
 
-axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
+function handleSuccessResponse(response){
+  const { success, message ,data } = response.data
+    if(success) {
+      toast.success(message)
+      return data;
+    }
+}
 
-    // Handle error
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
+function handleErrorResponse(error){
+  let message = ""
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
 
-      toast.error(error.response.message);
-    } else if (error.request) {
-      // The request was made but no response was received
+    message = error.response.message
+  } else if (error.request) {
+    // The request was made but no response was received
 
-      let moreInfo = "";
-      if (error.code = "ERR_NETWORK") {
-        moreInfo = "Server not reachable";
-      }
-
-      toast.error(error.message + ": " + moreInfo);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-
-      toast.error('Something went wrong: ', error.message);
-
+    let moreInfo = "";
+    if (error.code === "ERR_NETWORK") {
+      moreInfo = "Server not reachable";
     }
 
+    message = error.message + ": " + moreInfo
+  } else {
+    // Something happened in setting up the request that triggered an Error
 
-  },
+    message = 'Something went wrong: ' + error.message
+  }
+  toast.error(message)
+  return null;
+}
+
+axiosInstance.interceptors.response.use(
+    (response) => { handleSuccessResponse(response) },
+    (error) => { handleErrorResponse(error) },
 );
 
 export default axiosInstance;
