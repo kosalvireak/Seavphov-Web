312ebf21fffd5969cdb31650e43f4fdbfc4392c8
Kosalvireak <virakvary@gmail.com>
2025-04-04 00:03:41 +0700

    feat: adding pdf file and and unread column to db

---
+++ backend/database/migrations/2025_04_03_164800_add_file_name_and_unread_to_notification_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->boolean('unread')->default(false);
+        });
+        Schema::table('books', function (Blueprint $table) {
+            $table->text('pdf_filename')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->dropColumn('unread');
+        });
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropColumn('pdf_filename');
+        });
+    }
+};
