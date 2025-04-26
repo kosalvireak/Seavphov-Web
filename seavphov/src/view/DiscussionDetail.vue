a957b7ad78523e8ba14d23b65b9881345a1ab66d
Kosalvireak <virakvary@gmail.com>
2025-04-26 14:29:53 +0700

    remove ads container

--- seavphov/src/view/DiscussionDetail.vue
+++ seavphov/src/view/DiscussionDetail.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="DiscussionDetail min-h-screen grid grid-cols-12 gap-4 w-100">
-    <AdsContainer class="bg-gray-400" />
-    <div class="col-span-12 lg:col-span-6 my-4 space-y-6 container-xl">
+    <!-- <AdsContainer class="bg-gray-400" /> -->
+    <div class="col-span-12 lg:col-span-9 my-4 space-y-6 container-xl">
       <BackRoute />
       <div class="flex-center w-100 h-44" v-if="isLoading">
         <Loader :size="40" />
@@ -11,7 +11,7 @@
         <CommentSection :discussion_id="paramsId" />
       </template>
     </div>
-    <AdsContainer class="bg-green-400" />
+    <AdsContainer class="col-span-12 lg:col-span-3 bg-green-400" />
   </section>
 </template>
 
