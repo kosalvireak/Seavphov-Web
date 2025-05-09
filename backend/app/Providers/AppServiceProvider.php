06613327b62ab8af014df6f7886f02919fabe31b
Kosal Vireak <kosalvireak.khoeun@khalibre.com>
2025-05-09 15:51:49 +0700

    update backend class

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -26,7 +26,9 @@ class AppServiceProvider extends ServiceProvider
             URL::forceScheme('https');
         }
 
-        app('router')->matched(function () use ($request) {
+         app('router')->matched(function ($event) {
+            $request = $event->request;
+
             $ip = $request->ip();
             $method = $request->method();
             $uri = $request->path();
