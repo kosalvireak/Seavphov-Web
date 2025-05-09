319ac10e068fab50a4d63b7877a38a7ef47ff908
Kosal Vireak <kosalvireak.khoeun@khalibre.com>
2025-05-09 14:33:40 +0700

    feat: add log between api

---
+++ backend/app/Http/Middleware/LogRequestInfo.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+
+use Closure;
+use Illuminate\Support\Facades\Log;
+
+class LogRequestInfo
+{
+    public function handle($request, Closure $next)
+    {
+        $ip = $request->ip();
+        $method = $request->method();
+        $uri = $request->getRequestUri();
+
+        Log::info("Request from IP: $ip - $method $uri");
+
+        return $next($request);
+    }
+}
--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +25,13 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
+
+        app('router')->matched(function ($route) {
+            $ip = Request::ip();
+            $method = Request::method();
+            $uri = Request::path();
+
+            Log::info("Access from IP: $ip - $method $uri");
+        });
     }
 }
