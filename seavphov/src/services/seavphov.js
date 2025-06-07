a146b4eaf0f2ba6d39fe4d916f02ad4f91ff0bbf
Kosalvireak <virakvary@gmail.com>
2025-06-07 10:59:33 +0700

    remove image usage from github

--- seavphov/src/services/seavphov.js
+++ seavphov/src/services/seavphov.js
@@ -3,7 +3,7 @@ import { signInMethods } from "../utils/requireSignin";
 
 export default class Seavphov {
   static logoUrl =
-    "https://raw.githubusercontent.com/kosalvireak/Seavphov-Web/refs/heads/vue/assets/Seavphov%20Logo-2.png";
+    "https://firebasestorage.googleapis.com/v0/b/seavphov-919d7.appspot.com/o/folder%2FSeavphov%20Logo-2-min.png?alt=media&token=9766ac82-0403-4c08-ab32-43bb61e27b0e";
   static maxRelatedBook = 5;
   static defaultCopProfile =
     "https://static.vecteezy.com/system/resources/previews/054/453/530/non_2x/proactive-community-engagement-icon-vector.jpg";
