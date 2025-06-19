9304c3458c70e55ced0cc8369e7db7331840f7d1
Kosalvireak <virakvary@gmail.com>
2025-06-19 14:03:50 +0700

    fix swagger

--- backend/app/Http/Controllers/CopMemberController.php
+++ backend/app/Http/Controllers/CopMemberController.php
@@ -147,23 +147,6 @@ class CopMemberController extends Controller
         }
     }
 
-    /**
-     * @OA\Get(
-     *     path="/api/community/{route}/members",
-     *     summary="Get community's member by route (display in home)",
-     *     tags={"Community's Member"},
-     *     security={{"bearerAuth":{}}},
-     *     @OA\Parameter(
-     *         name="route",
-     *         in="path",
-     *         required=true,
-     *         description="Route of the community",
-     *         @OA\Schema(type="string", default="1mmee")
-     *     ),
-     *     @OA\Response(response=200, description="Get Community members successfully"),
-     *     @OA\Response(response=500, description="Cannot get Community members!")
-     * )
-     */
     public function getHomeCommunityMembersList($route)
     {
         try {
